e03753d4916464a00c1dcdd08ebcb1b59c071014
Bruce Kropp <bruce.kropp@raytheon.com>
2022-08-19 23:42:58 +0000

    add internal SRW support function to determine e2e test state

--- .cicd/scripts/srw_functions.sh
+++ .cicd/scripts/srw_functions.sh
@@ -37,33 +37,53 @@ function SRW_has_cron_entry() # Are there any srw-build-* experiment crons runni
     crontab -l | grep "ufs-srweather-app/srw-build-${SRW_COMPILER:-"intel"}/expt_dirs/$dir"
 }
 
+function SRW_wflow_status() # [internal] used to determine state of an e2e test
+{
+    local log_data="$1"
+    local result=""
+    local rc=0
+    
+    result=$(echo "$log_data" | cut -d: -f2-)
+    if [[ 0 == $? ]]; then
+        rc=1
+        echo "$result" | egrep -i 'IN PROGRESS|SUCCESS|FAILURE' > /dev/null || result=PENDING
+        [[ $result =~ PROGRESS ]] && rc=1
+        [[ $result =~ SUCCESS ]] && rc=0
+        [[ $result =~ FAILURE ]] && rc=0
+    else
+        result="Not Found" && rc=9
+    fi
+    echo "$result"
+    return $rc
+}
+
 function SRW_get_details() # Use rocotostat to generate detailed test results
 {
-  local startTime="$1"
-  local opt="$2"
-  local log_file=""
-  local workspace=${WORKSPACE:-"."}
-  echo ""
-  echo "#### started $startTime"
-  echo "#### checked $(date)"
-  echo "#### ${SRW_COMPILER}-${PLATFORM:-"${NODE_NAME,,}"} ${JOB_NAME:-$(git config --get remote.origin.url 2>/dev/null)} -b ${GIT_BRANCH:-$(git symbolic-ref --short HEAD 2>/dev/null)}"
-  echo "#### rocotostat -w "FV3LAM_wflow.xml" -d "FV3LAM_wflow.db" -v 10 $opt"
-  echo ""
-  for dir in $(cat ${WORKSPACE}/regional_workflow/tests/WE2E/expts_file.txt 2>/dev/null) ; do
-    log_file=$(cd ${WORKSPACE}/expt_dirs/$dir/ 2>/dev/null && ls -1 log.launch_* 2>/dev/null)
-    (
-    echo "# rocotostat $dir/$log_file:"
-    cd ${WORKSPACE}/expt_dirs/$dir/ && rocotostat -w "FV3LAM_wflow.xml" -d "FV3LAM_wflow.db" -v 10 $opt 2>/dev/null
+    local startTime="$1"
+    local opt="$2"
+    local log_file=""
+    local workspace=${WORKSPACE:-"."}
     echo ""
-    )
-  done
-  echo "####"
+    echo "#### started $startTime"
+    echo "#### checked $(date)"
+    echo "#### ${SRW_COMPILER}-${PLATFORM:-"${NODE_NAME,,}"} ${JOB_NAME:-$(git config --get remote.origin.url 2>/dev/null)} -b ${GIT_BRANCH:-$(git symbolic-ref --short HEAD 2>/dev/null)}"
+    echo "#### rocotostat -w "FV3LAM_wflow.xml" -d "FV3LAM_wflow.db" -v 10 $opt"
+    echo ""
+    for dir in $(cat ${WORKSPACE}/regional_workflow/tests/WE2E/expts_file.txt 2>/dev/null) ; do
+        log_file=$(cd ${WORKSPACE}/expt_dirs/$dir/ 2>/dev/null && ls -1 log.launch_* 2>/dev/null)
+        (
+        echo "# rocotostat $dir/$log_file:"
+        cd ${WORKSPACE}/expt_dirs/$dir/ && rocotostat -w "FV3LAM_wflow.xml" -d "FV3LAM_wflow.db" -v 10 $opt 2>/dev/null
+        echo ""
+        )
+    done
+    echo "####"
 }
 
 function SRW_save_tests() # Save SRW E2E tests to persistent storage, cluster_noaa hosts only 
 {
     local SRW_SAVE_DIR="$1"
-        echo "#### Saving SRW tests to ${SRW_SAVE_DIR}/${NODE_NAME}/$day_of_week/expt_dirs.tar"
+    echo "#### Saving SRW tests to ${SRW_SAVE_DIR}/${NODE_NAME}/$day_of_week/expt_dirs.tar"
     [[ -n ${SRW_SAVE_DIR} ]] && [[ -d ${SRW_SAVE_DIR} ]] || return 1
     [[ -n ${NODE_NAME} ]] || return 2
     if [[ ${NODE_NAME} =~ cluster_noaa ]] && [[ -d ${SRW_SAVE_DIR} ]] ; then
